943c6d06e200a2f39028a153a4e2ac7ad20b7689
Luke Stahl <Stahl1109@gmail.com>
2017-12-18 17:50:28 -0600

    gem buttons working

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -10,63 +10,39 @@ $(document).ready(function(){
     var gemTwo ="button-2";
     var gemThree = "button-3";
     var gemFour = "button-4";
-    var totalScore = 0;
+    // var totalScore = 0;
     var isScore = true;
     
+
     document.getElementById("randomNumGenerator").innerHTML = Math.floor(Math.random() * 120) +19;
+   
+    var targetNumber = "randomNumGenerator";
+
+    $("randomNumGenerator").text(targetNumber);
+
+    var counter = 0
    
     // var targetNum = randomNum[Math.floor(Math.random() * randomNum.length)];
 
-$("#button-1" ).on("click", function(){
-    var crystalValue =($(this).attr("data-crystalvalue"));
-    crystalValue = parseInt(crystalValue);
-
-    counter += crystalValue
+$("#button-1").on("click", function(){
+    document.getElementById("totalScore").innerHTML = Math.floor(Math.random() * 12) +1;
 });
 
-$("#button-2" ).on("click", function(){
-    var crystalValue =($(this).attr("data-crystalvalue"));
-    crystalValue = parseInt(crystalValue);
-
-    counter += crystalValue
+$("#button-2").on("click", function(){
+    document.getElementById("totalScore").innerHTML = Math.floor(Math.random() * 12) +1;
 });
 
-$("#button-3" ).on("click", function(){
-    var crystalValue =($(this).attr("data-crystalvalue"));
-    crystalValue = parseInt(crystalValue);
-
-    counter += crystalValue
+$("#button-3").on("click", function(){
+    document.getElementById("totalScore").innerHTML = Math.floor(Math.random() * 12) +1;
 });
 
-$("#button-4" ).on("click", function(){
-    var crystalValue =($(this).attr("data-crystalvalue"));
-    crystalValue = parseInt(crystalValue);
-
-    counter += crystalValue
+$("#button-4").on("click", function(){
+    document.getElementById("totalScore").innerHTML = Math.floor(Math.random() * 12) +1;
 });
 
+// document.getElementById("totalScore").innerHTML = crystalValue;
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-  
-   
-
-
-
-   
-// });
+});
 
 // var totalScore = function (score) {
 //     if (score < targetNum) {
